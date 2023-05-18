133b4533ef9d4b9f2f1609455e6df71b4f01ff8b
vinay4669 <115799707+vinay4669@users.noreply.github.com>
2023-05-18 09:34:54 +0530

    mvps 2

--- mvps.php
+++ mvps.php
@@ -29,3 +29,21 @@ $bg = array('ap'=>'A+', 'an'=>'A-', 'bp'=>'B+', 'bn'=>'B-', 'abp'=>'AB+', 'abn'=
         </table>
     </div>
 </main>
+
+<footer class="footer">
+    <div class="heading">
+        JOIN OUR CAUSE
+    </div>
+    <div class="content">
+        Donating blood or platelets can be intimidating and even scary. 
+        Time to put those hesitations and fears aside. Learn from Little Blood  
+        how simple and easy it is to roll up a sleeve and help save lives.
+    </div>
+    <!-- <div class="links">
+
+    </div> -->
+    <div class="foot">
+        2023 &copy; All rights reserved.
+    </div>
+    
+</footer>
